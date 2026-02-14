e771318852cc32b6ea46068d6efefb7b5df57703
notisalomon <sulejmani.labinot@gmail.com>
2026-02-14 23:24:53 +0100

    Added comments to explain the magic code

--- src/MatheMagic.java
+++ src/MatheMagic.java
@@ -1,5 +1,8 @@
 public class MatheMagic {
     public static void main(String[] args) {
+        /* To see the magic happen, change the value of myNumber to any number but 0 and run it.
+        Check the result and now change the value of myNumber again and run the program again.
+        See the magic happen!! (Fireworks in front of your eyes) */
         int myNumber = 5;
         int stepOne = myNumber * myNumber;
         int stepTwo = stepOne + myNumber;
